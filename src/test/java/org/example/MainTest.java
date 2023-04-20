cef0cd56a62dd0190d1b0c2ad99c802e70d86b3d
BenjaminCruzado <b.cruzado01@ufromail.cl>
2023-04-20 16:17:05 -0400

    Subo 3 Pruebas unitarias

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -10,5 +12,26 @@ class MainTest {
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void testObtenerUltimoEspacio() {
+        int resultadoEsperado= 2;
+        String[][] registro = {{"benja","casado","23"},{"guga","soltero","21"},{null,null,null}};
+        assertEquals(Main.obtenerUltimoEspacio(registro),resultadoEsperado);
+    }
+
+    @Test
+    void testHayCupo() {
+        boolean resultadoEsperado= true;
+        String[][] registro = {{"benja","casado","23"},{"guga","soltero","21"},{null,null,null}};
+        assertEquals(Main.hayCupo(registro),resultadoEsperado);
+    }
+
+    @Test
+    void testOpa() {
+        int resultadoEsperado= 1;
+        String[][] registro = {{"benja","casado","23"},{"guga","soltero","21"},{null,null,null}};
+        assertEquals(Main.opa(registro),resultadoEsperado);
     }
 }
